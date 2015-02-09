e3c35e1e788998787db9d608cbf308351ad37e8d
Don Okuda <don.okuda@gmail.com>
2015-02-08 18:53:46 -0800

    Add Password strength meter

--- dropbox/CreateAccountViewController.swift
+++ dropbox/CreateAccountViewController.swift
@@ -11,11 +11,24 @@ import UIKit
 class CreateAccountViewController: UIViewController {
 
     @IBOutlet weak var backButton: UIButton!
+    
+    @IBOutlet weak var passwordHintText: UILabel!
+    @IBOutlet weak var passwordHintView: UIView!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBOutlet weak var goodPassIndicator: UIView!
+    @IBOutlet weak var greatPassIndicator: UIView!
+    @IBOutlet weak var okPassIndicator: UIView!
+    @IBOutlet weak var weakPassIndicator: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        // Do any additional setup after loading the view.
+        passwordHintView.hidden = true
+        goodPassIndicator.hidden = true
+        greatPassIndicator.hidden = true
+        okPassIndicator.hidden = true
+        weakPassIndicator.hidden = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -36,5 +49,61 @@ class CreateAccountViewController: UIViewController {
 
     @IBAction func backButtonTapped(sender: AnyObject) {
         navigationController!.popViewControllerAnimated(true)
+    }
+    
+    
+    @IBAction func onPasswordEdit(sender: AnyObject) {
+        let weakColor = UIColor(red: 0.937, green: 0, blue: 0.071, alpha: 1)
+        let okColor = UIColor(red: 0.957, green: 0.463, blue: 0.102, alpha: 1)
+        let goodColor = UIColor(red: 0.988, green: 0.761, blue: 0.043, alpha: 1)
+        let greatColor = UIColor(red: 0.098, green: 0.722, blue: 0.02, alpha: 1)
+        
+        switch passwordField.text.utf16Count {
+        case 0:
+            goodPassIndicator.hidden = true
+            greatPassIndicator.hidden = true
+            okPassIndicator.hidden = true
+            weakPassIndicator.hidden = true
+            
+            passwordHintView.hidden = true
+        case 1...4:
+            weakPassIndicator.hidden = false
+            okPassIndicator.hidden = true
+            goodPassIndicator.hidden = true
+            greatPassIndicator.hidden = true
+            
+            passwordHintView.hidden = false
+            
+            weakPassIndicator.backgroundColor = weakColor
+            passwordHintText.textColor = weakColor
+            passwordHintText.text = "Weak"
+        case 5...8:
+            weakPassIndicator.hidden = false
+            okPassIndicator.hidden = false
+            goodPassIndicator.hidden = true
+            greatPassIndicator.hidden = true
+            
+            okPassIndicator.backgroundColor = okColor
+            passwordHintText.textColor = okColor
+            passwordHintText.text = "So-So"
+        case 9...12:
+            weakPassIndicator.hidden = false
+            okPassIndicator.hidden = false
+            goodPassIndicator.hidden = false
+            greatPassIndicator.hidden = true
+            
+            goodPassIndicator.backgroundColor = goodColor
+            passwordHintText.textColor = goodColor
+            passwordHintText.text = "Good"
+        default:
+            weakPassIndicator.hidden = false
+            okPassIndicator.hidden = false
+            goodPassIndicator.hidden = false
+            greatPassIndicator.hidden = false
+            
+            greatPassIndicator.backgroundColor = greatColor
+            passwordHintText.textColor = greatColor
+            passwordHintText.text = "Great!"
+        }
     }
 }
